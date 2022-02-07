659c693a7ca4af93db400d358c3c5d851277bda2
Sami00002 <samuel.bodogan@emanuel.ro>
2022-02-07 10:03:05 +0200

    Add project files.

---
+++ SumaElementelor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SumaElementelor
+{
+    class Program
+    {   
+        public int Suma(int[] array,int n)
+        {
+            if (n == 0)
+                return 0;
+            else
+             return array[n] + Suma(array[n - 1]);
+           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cate numei vrei sa citesti de la tastatura? ");
+            int nrElements = int.Parse(Console.ReadLine());
+            int[] array = new int[nrElements];
+
+            for (int i = 0; i < nrElements; i++)
+            {
+                Console.Write($"The {i + 1}. element = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < nrElements; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+           
+        }
+       
+    }
+}
